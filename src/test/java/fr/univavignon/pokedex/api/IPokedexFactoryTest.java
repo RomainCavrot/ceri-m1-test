3acd255db73e3c1fc3fab293e673391244cb1a8c
Rhums <romaincavrot@hotmail.fr>
2017-05-05 10:28:29 +0200

    add test class

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,11 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Test;
+
+public class IPokedexFactoryTest {
+
+    @Test
+    public void testCreatePokedex() {
+    	
+    }
+}
---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -0,0 +1,16 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Test;
+
+public class IPokedexTest {
+
+    @Test
+    public void testAddPokemon() {
+
+    }
+
+    @Test
+    public void testGetPokemon() {
+
+    }
+}
